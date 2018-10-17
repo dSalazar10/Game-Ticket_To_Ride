eb02403b73fe0c6e757b64081b11acd8edc7e726
PC1 <PC1@134.154.20.192>
2018-10-17 16:27:39 -0700

    PlayerClass Skelton

--- src/model/Player.java
+++ src/model/Player.java
@@ -14,45 +14,65 @@ public class Player {
     List<TrainPieces> pieces;
     List<TrainCar> carCards;
     int points;
-    public player(){
+    
+    public void player(){
         pieces = new ArrayList<>();
         carCards = new ArrayList<>();
         points = 0;
     }
+    /*
     public TrainPieces getTrainPieces(){
         //tbd
     }
+    */
     public boolean insertTrainPiece(){
         //tbd
+        return false;
     }
     public boolean removeTrainPiece(){
         //tbd
+        return false;
     }
+    /*
     public TrainCar getTrainCar(){
         //tbd
     }
+    */
     public boolean insertTrainCar(){
         //tbd
+        return false;
     }
     public boolean removeTrainCar(){
         //tbd
+        return false;
     }
+    /*
     public DestTixCard getDestTixCard(){
         //tbd
     }
+    */
     public boolean insertDestTixCard(){
         //tbd
+        return false;
     }
     public boolean removeDestTixCard(){
         //tbd
+        return false;
     }
     public boolean addPoints(int a){
+        int oldpoints = points;
         points += a;
+        if (points > oldpoints) return true;
+        else return false;
     }
     public boolean subPoints(int s){
+        int oldpoints = points;
         points -= s;
+        if (points < oldpoints) return true;
+        else return false;
     }
     public boolean turn(){
         //tbd 
+        return false;
     }
 }
