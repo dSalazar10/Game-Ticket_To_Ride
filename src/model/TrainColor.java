f945644439e2dd7f52a00f7936fd55b831cf82c8
Nasario S <nasario.ita@gmail.com>
2018-10-21 16:07:41 -0700

    Fully implemneted TrainCard (it only needed vars) Fixed errors in TrainCardDeck, and initialized the deck. Added enums for color and trains
    Signed-off-by: Nasario S <nasario.ita@gmail.com>

---
+++ src/model/TrainColor.java
@@ -0,0 +1,13 @@
+/*
+ * FileName.java
+ * Version 0.1.4    : Setup
+ * Programmer       : Nasario Sylvester
+ * Due Date         : 10/24/18
+ * Last Modified    : MM/DD/YY HH:HH
+ */
+
+package model;
+// enum synched with TrainType
+public enum TrainColor{
+    Rainbow, Pink, White, Blue, Yellow, Orange, Black, Red, Green;
+}
