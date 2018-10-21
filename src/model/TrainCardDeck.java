631c02dfc036ed4b9e53e5d75810201ad836df7e
Nasario S <nasario.ita@gmail.com>
2018-10-21 16:03:58 -0700

    Fully implemneted TrainCard (it only needed vars) Fixed errors in TrainCardDeck, and initialized the deck. Added enums for color and trains

--- src/model/TrainCardDeck.java
+++ src/model/TrainCardDeck.java
@@ -1,32 +1,61 @@
-import java.util.*;
 package model;
+import java.util.LinkedList;
 
-public class TrainCardDeck{
+
+
+public class TrainCardDeck {
 	//deck
-	private ArrayList<TrainCarCard> trainDeck;
+	private LinkedList<TrainCard> trainDeck;
 	//discard pile
-	private LinkedList<TrainCarCard> trainDiscard;
+	private LinkedList<TrainCard> trainDiscard;
 	//cards on the table
-	private LinkedList<TrainCarCard> tableCards;
+	private LinkedList<TrainCard> tableCards;
 	//placeholder for now
-	private TrainCarCard card;
+	private TrainCard card;
+
 	//contruusttor will t
-	TrainCardDeck(){
-		trainDeck.Shuffle();
+	TrainCardDeck() {
+		trainDeck = new LinkedList<TrainCard>();
+		int i = 0;
+		// creates the locomotive cards
+		// and adds them to the end of the deck
+		while(i < 14){
+			trainDeck.add(new TrainCard(0));
+			i++;
+		}
+		i = 0;
+		int j = 0;
+		// Creates all cards and add them to the deck end of the deck
+
+		while(j < 8){
+			while(i < 12){
+				trainDeck.add(new TrainCard(j));
+				i++;
+			}
+			j++;
+		}
+		this.Shuffle();
 	}
+
 	private void Shuffle() {
 
 	}
+
 	//test if player wants cards from deck or table and takes the action
-	public TrainCarCard Draw(Boolean t) {
-		if(t)
-			return trainDiscard.addFrist(trainDeck.pop());
+	public TrainCard Draw(Boolean t) {
+		if (t)
+			return trainDeck.pop();
 		else
-			return this.toTable(int pos);
-	}
-	// giving the player a card the card they want from the table
-	public TrainCarCard(int pos) {
-		return card;
+			return this.toTable(1);
 	}
 
+	// giving the player a card the card they want from the table
+	public TrainCard fromTable(int pos) {
+		return tableCards.get(pos);
+	}
+
+
+	public TrainCard toTable(int pos) {
+		return new TrainCard(1);
+	}
 }
