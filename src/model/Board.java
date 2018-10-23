7e699f196a0e6de79a1047f5574472a7a34b883e
castle <dsalazar10@horizon.csueastbay.edu>
2018-10-23 05:23:14 -0700

    Merge branch 'dsalazar10' of /home/castle/IdeaProjects/Ticket_To_Ride with conflicts.

--- src/model/Board.java
+++ src/model/Board.java
@@ -15,18 +15,12 @@ package model;
 public class Board {
 
     // Fake prototypes
-    private class Destination_Ticket_Deck {
-        private void Shuffle() {}
-    }
-    private class Train_Car_Discard {
-    }
-    private class Train_Car_Faceup {
-    }
     private class Longest_Path {
     }
 
     // Variables
-    TrainCardDeck deck;
+    TrainCardDeck trainDeck, discard, faceup;
+    DestinationTicketSet destDeck;
     String routes[] = new String[] {"Vancouver","Calgary","Winnipeg","Seattle","Portland","Helena","Salt Lake City",
             "Denver","San Francisco","Las Vegas","Santa Fe","Los Angeles","Phoenix","El Paso","Oklahoma City",
             "Duluth","Omaha","Kansas City","Dallas","Houston","Chicago","Saint Louis","Little Rock","New Orleans",
@@ -36,35 +30,30 @@ public class Board {
     // Default Constructor
     public Board() {
         // Fake prototypes
-        Init_Destination_Cards();
-        Init_Discard_Cards();
-        Init_Face_Up_Cards();
         Init_Longest_Path_Card();
 
         // Variables
         initTrainCardCards();
+        initDestinationDeck();
+        initDiscardCards();
+        initFaceUpCards();
     }
 
     // Fake prototypes
-    private void Init_Destination_Cards() {
-        Destination_Ticket_Deck dtd = new Destination_Ticket_Deck();
-        dtd.Shuffle();
-    }
-    private void Init_Discard_Cards() {
-        Train_Car_Discard tcd = new Train_Car_Discard();
-    }
-    private void Init_Face_Up_Cards() {
-        Train_Car_Faceup tcf = new Train_Car_Faceup();
-    }
     private void Init_Longest_Path_Card() {
         Longest_Path lp = new Longest_Path();
     }
 
     // Initialize the Train Car Card Deck and Shuffle
-    private void initTrainCardCards() {
-        // Variables
-        deck = new TrainCardDeck();
+    private void initTrainCardCards() { trainDeck = new TrainCardDeck(); }
+    private void initDestinationDeck() {
+        destDeck = new DestinationTicketSet();
     }
-
+    private void initDiscardCards() {
+        discard = new TrainCardDeck();
+    }
+    private void initFaceUpCards() {
+        faceup = new TrainCardDeck();
+    }
 
 }
