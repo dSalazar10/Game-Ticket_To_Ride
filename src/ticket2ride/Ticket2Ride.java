ef3413f3bccd1b37a8897b705f3ec3568f40af10
NasarioS <44071205+NasarioS@users.noreply.github.com>
2018-11-03 00:04:56 -0700

    Update Ticket2Ride.java

--- src/ticket2ride/Ticket2Ride.java
+++ src/ticket2ride/Ticket2Ride.java
@@ -80,7 +80,7 @@ public class Ticket2Ride {
             List<TrainCard> trainCards = board.get2TrainCards();
             return trainCards;
         } else {
-            List<TrainCard> destCards = board.get2TrainCards();
+            List<TrainCard> destCards = board.get3DestCards();
             return destCards;
         }
         */
