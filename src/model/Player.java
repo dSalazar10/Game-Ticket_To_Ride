c58f7d90a4433bff3a0f05320b71bff976f32ec3
Mona Lisa <adam.hennefer@gmail.com>
2018-11-08 21:09:16 -0800

    Player Class setup 0.1.6

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,9 +1,9 @@
 /*
  * Player.java
- * Version 0.1.5    : Setup
+ * Version 0.1.6    : Setup
  * Programmer       : Adam Hennefer
- * Due Date         : 11/7/18
- * Last Modified    : 10/31/18 12:52
+ * Due Date         : 11/9/18
+ * Last Modified    : 11/8/18 2:52
  *
  * This class represents a player.
  *  
@@ -23,7 +23,7 @@ public class Player {
     private TrainPieces.COLOR pColor;
     private boolean turn = false;
     
-    // construct player object
+// construct player object
     public Player(String n, TrainPieces.COLOR c){
         //set player's name
         name = n;
@@ -33,47 +33,40 @@ public class Player {
         
         //initialize ArrayList of forty five train pieces with player's color
         pieces = new TrainPieces().getPieces(c); 
-        
-        //initialize ArrayList of four traincar cards        
-//        carCards = new ArrayList<>();
-//        TrainCardDeck deck = new TrainCardDeck();
-//        carCards = deck.toPlayer();
-
-        //initialize ArrayList of three destination cards    
-        //destCards = new ArrayList<>();
-        //DestinationTicketSet dCard = new DestinationTicketSet();
-        //destCards = dCard.Draw(destCards);
-                
+ 
         //initialize points set to zero    
-        points = 0;
-        
+        points = 0;      
     }
-    
+ 
     public boolean addBoard(Board b){
         board = b;
         carCards =  b.trainDeck.toPlayer();
         destCards = b.destDeck.toPlayer();
-        return board == b;
+        return(board == b && carCards ==  b.trainDeck.toPlayer() && destCards == b.destDeck.toPlayer());
     }
    
     public TrainPiece getTrainPiece(int p){
         return pieces.get(p);
     }
     
-    //public boolean insertTrainPiece(TrainPiece tp){
-    public boolean insertTrainPiece(Board r){
-//        int routeLength  = r.routes.length;
-//        String route[] = r.routes;
-//        int i = 0;
-//        if (r.claimRoute){
-//            while(i < routeLength){
-//                TrainPiece tp = pieces.get(i);
+    // insert a TrainPiece into a newly claimed route
+    public boolean insertTrainPiece(DestinationCard dc){
+        // if the DestinationCard route(s) are claimed or we don't have enough train pieces then return false
+//        if(board.claimRoute(dc.getRouteA()) && board.claimRoute(dc.getRouteB()) || pieces.size() < dc.getPts()){
+//            return false; }
+//        else we are going to claim the route and throw out an equivlent amount of train pieces
+//         to the routes path (aka pts)
+//        else{
+//          if(board.claimRoute(dc.getRouteA()){
+//                
+//        
+//            board.claimRoute(dc.getRouteA());
+//            int i = 0;
+//            while(i < dc.getPts()){
 //                pieces.remove(i);
-//                //r.routes.add()......
-//                //tbd
-//            }
+//            }    
 //        }
-        return false;
+        return true;
     }
     //retrives a train piece of the players list of train pieces
     public void removeTrainPiece(int p){
@@ -89,26 +82,30 @@ public class Player {
     public boolean insertTrainCar(TrainCard tc){
         return carCards.add(tc); 
     }
-    // removes a train car card from players list of train cars
-    public void removeTrainCar(int tc){
-        carCards.remove(tc);   
+    // removes a train car card from player's list of train cars
+    public boolean removeTrainCar(int tc){
+        TrainCard temp = carCards.get(tc);
+        return carCards.remove(temp);
     }
     
-    // returns the players current destinatoin cards
+    // returns the players current list destination cards
     public ArrayList<DestinationCard> getDestTixCard(){
         return this.destCards;
+    }
+    // returns the players current list car train cards
+    public ArrayList<TrainCard> getTrainCards(){
+        return this.carCards;    
     }
     
     // add a destination card to the players list of destination cards
     public boolean insertDestTixCard(DestinationCard dc){
         return destCards.add(dc);
     }
-    
-    // removes a destinatoin card from the plaers list of destination cards
+    // removes a destinatoin card from the player's list of destination cards
     public boolean removeDestTixCard(int cNum){
-       DestinationCard temp = destCards.get(cNum);
+        DestinationCard temp = destCards.get(cNum);
         destCards.remove(cNum);
-       board.destDeck.retunTo(temp);
+        board.destDeck.retunTo(temp);
         return true;
     }
     //add player points
@@ -142,6 +139,6 @@ public class Player {
             Player y = (Player)x;
             return this.name.equals(y.name) && this.pColor == y.pColor;
 	}
-	public int getPeicesLeft(){ return pieces.size(); }
-	public int getSizeofDest(){ return destCards.size(); }
+    public int getPeicesLeft(){ return pieces.size(); }
+    public int getSizeofDest(){ return destCards.size(); }
 }
